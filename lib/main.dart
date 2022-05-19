f560694a7b5af0c204a3ca04cf8d3d3bdd871af1
egyleader <ahmed.os.attya@gmail.com>
2022-05-19 17:59:54 +0200

    added app logic to main.dart

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text(
+            '${ kIsWeb ? "" : Platform.operatingSystem} Device Info ',
+            style: Theme.of(context).textTheme.headline4,
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.8,
+          child: FutureBuilder<BaseDeviceInfo>(
+              future: deviceInfo.deviceInfo,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) return const CircularProgressIndicator();
+                var info = snapshot.data!.toMap();
+                return ListView.builder(
+                    itemCount: info.length,
+                    itemBuilder: (ctx, i) => ListTile(
+                            title: RichText(
+                          text: TextSpan(
+                              text: '${info.keys.elementAt(i)} : ',
+                              style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.deepPurpleAccent),
+                              children: [
+                                TextSpan(
+                                    text: info[info.keys.elementAt(i)].toString(),
+                                    style: const TextStyle(color: Colors.black, fontWeight: FontWeight.normal))
+                              ]),
+                        )));
+              }),
+        )
+      ]),
+    );
+  }
+}
